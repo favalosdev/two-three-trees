0c3ad7795d2c5381bfa4d54ea50efd042df20a9b
Fernando Avalos <fernandoadev@protonmail.com>
2025-10-25 14:45:34 -0500

    Nit

--- tests/test_well_ordering.rs
+++ tests/test_well_ordering.rs
@@ -5,8 +5,8 @@ use util::tree::verify_well_ordering;
 
 #[test]
 fn check_well_ordering() {
-    assert_eq!(verify_well_ordering(ex_1()), false);
-    assert_eq!(verify_well_ordering(ex_2()), true);
-    assert_eq!(verify_well_ordering(ex_3()), true);
-    assert_eq!(verify_well_ordering(ex_4()), true);
+    assert_eq!(verify_well_ordering(&ex_1()), false);
+    assert_eq!(verify_well_ordering(&ex_2()), true);
+    assert_eq!(verify_well_ordering(&ex_3()), true);
+    assert_eq!(verify_well_ordering(&ex_4()), true);
 }
